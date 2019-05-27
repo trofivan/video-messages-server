77c1bbe3df66787c2a50a3c39466dd3ec812832d
Ivan Trofimov <ivan@trofimov.link>
2019-05-27 10:03:17 +0500

    master:working prototype

---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import http from 'http';
+import querystring from "querystring";
+import url from 'url';
+import WebSocket from 'ws';
+
+import { SERVER_PORT, WEBSOCKET_SERVER_PORT } from "./constants";
+
+const wss = new WebSocket.Server({port: WEBSOCKET_SERVER_PORT});
+
+wss.on('connection', (ws: WebSocket) => {
+    ws.on('message', (message: string) => {
+        console.log(`recieved: ${message}`);
+        ws.send(message);
+    });
+});
+
+const server = http.createServer((req, res) => {
+    const data = querystring.parse(url.parse(req.url).query);
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+
+    if (data.title && data.text) {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(data));
+            }
+        });
+    }
+
+    res.end(JSON.stringify(data));
+});
+
+
+server.listen(SERVER_PORT, () => {
+    console.log(`Server running at http://localhost:${SERVER_PORT}`);
+});
+
+export default server;
